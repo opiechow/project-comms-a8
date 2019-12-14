6a53797b4eb90a4333a8cc2001fb8ceb4ef3aff6
Oskar Piechowski <ospiechowski@gmail.com>
2019-12-14 21:35:33 +0100

    Various cosmetic changes
    Make the code look prettier

--- uart_handler.c
+++ uart_handler.c
@@ -29,9 +29,13 @@ volatile uint8_t c_len = 0;
 /** Interrupt Service Routine for Receive Complete
  *  Drops incoming chars if buffer full
  */
-ISR(USART_RXC_vect) {
-  if (c_len < UART_BUF_SIZE) {
-    in_buffer[c_len++] = UDR;
+ISR(USART_RXC_vect)
+{
+  in_buffer[c_len] = UDR;
+  c_len += 1;
+  if (c_len >= UART_BUF_SIZE)
+  {
+    c_len = 0;
   }
 }
 
@@ -39,7 +43,8 @@ ISR(USART_RXC_vect) {
  *   Default frame format is 8 data bits, no parity, 1 stop bit
  *   to change use UCSRC, see AVR datasheet
  */
-void USART_Init(void) {
+void USART_Init(void)
+{
   /* Load lower 8 - bits into the low byte of the UBRR register */
   UBRRL = BAUD_PRESCALE;
   /* Load upper 8-bits into the high byte of the UBRR register */
@@ -48,9 +53,11 @@ void USART_Init(void) {
   UCSRB = ((1 << TXEN) | (1 << RXEN) | (1 << RXCIE));
 }
 
-void USART_SendByte(uint8_t u8Data) {
+void USART_SendByte(uint8_t u8Data)
+{
   // Wait until last byte has been transmitted
-  while ((UCSRA & (1 << UDRE)) == 0) {
+  while ((UCSRA & (1 << UDRE)) == 0)
+  {
     ;
   }
 
@@ -58,7 +65,8 @@ void USART_SendByte(uint8_t u8Data) {
   UDR = u8Data;
 }
 
-void portb_init(void) {
+void portb_init(void)
+{
   // outputs, all off
   DDRB = 0xFF;
   PORTB = 0xFF;
@@ -76,7 +84,8 @@ static inline double baud_to_us(int baudrate) { return 1000000 / baudrate; }
  * Configured via #SOFTUART_PORT, #SOFTUART_PIN and $SOFTUART_BAUDRATE
  * definitions. Besides we have hardcoded no parity and 1 stop bit for now.
  */
-void softuart_send_byte(uint8_t tx_byte) {
+void softuart_send_byte(uint8_t tx_byte)
+{
   int8_t i;
 
   /* Send the start bit */
@@ -84,10 +93,14 @@ void softuart_send_byte(uint8_t tx_byte) {
   _delay_us(baud_to_us(SOFTUART_BAUDRATE));
 
   /* Transfer the byte consisting of 8 bits */
-  for (i = 0; i < 8; i++) {
-    if (CHECK_BIT(tx_byte, i)) {
+  for (i = 0; i < 8; i++)
+  {
+    if (CHECK_BIT(tx_byte, i))
+    {
       SET_BIT(SOFTUART_PORT, SOFTUART_PIN);
-    } else {
+    }
+    else
+    {
       CLR_BIT(SOFTUART_PORT, SOFTUART_PIN);
     }
     _delay_us(baud_to_us(SOFTUART_BAUDRATE));
@@ -98,57 +111,72 @@ void softuart_send_byte(uint8_t tx_byte) {
   _delay_us(baud_to_us(SOFTUART_BAUDRATE));
 }
 
-static inline void clear_buffer(void *buffer) {
-  memset(buffer, 0, UART_BUF_SIZE);
-  if (buffer == in_buffer) {
+static inline void clear_in_buffer()
+{
+  ATOMIC_BLOCK(ATOMIC_FORCEON)
+  {
+    memset(in_buffer, 0, UART_BUF_SIZE);
     c_len = 0;
   }
 }
 
-static inline void send_string(uint8_t *str, void (*char_tx)(uint8_t)) {
-  while (*str) {
+static inline void send_string(uint8_t *str, void (*char_tx)(uint8_t))
+{
+  while (*str)
+  {
     char_tx(*str);
     str++;
   }
 }
 
-static void esp_wait_for_ok() {
+static void esp_wait_for_ok(unsigned int timeout, uint8_t num_retries)
+{
   uint8_t *found;
   uint8_t offset;
-  for (;;) {
+  uint8_t i;
+
+  for (i = 0; i < num_retries; i++)
+  {
+    _delay_ms(timeout);
     found = strstr(in_buffer, "OK\r\n");
-    if (found) {
+    if (found)
+    {
       offset = found + 4 - in_buffer;
       memmove(in_buffer, found + 4, UART_BUF_SIZE - offset);
       c_len = c_len - offset;
       return;
     }
-    _delay_ms(1000);
   }
 }
 
-#define ESP_CONFIG_CMDS 2
-void send_esp_config() {
-  uint8_t *esp_init_cmds[ESP_CONFIG_CMDS] = {"AT+CIPMUX=1\r\n",
-                                             "AT+CIPSERVER=1,1234\r\n "};
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
+void send_esp_config()
+{
+  uint8_t *esp_init_cmds[] = {"AT+CIPMUX=1\r\n", "AT+CIPSERVER=1,1234\r\n "};
   uint8_t i;
 
-  for (i = 0; i < ESP_CONFIG_CMDS; i++) {
+  for (i = 0; i < ARRAY_SIZE(esp_init_cmds); i++)
+  {
+    clear_in_buffer();
     send_string(esp_init_cmds[i], USART_SendByte);
-    esp_wait_for_ok();
+    esp_wait_for_ok(500, 3);
   }
 }
 
 /* Returns filtered data len */
-int filter_data(uint8_t *dst_buff, uint8_t *src_buff) {
+int filter_data(uint8_t *dst_buff, uint8_t *src_buff)
+{
   uint8_t *colon;
   uint8_t *found;
   uint8_t offset;
 
   found = strstr(in_buffer, "OK\r\n");
-  if (found) {
+  if (found)
+  {
     colon = strchr(in_buffer, ':');
-    if (colon) {
+    if (colon)
+    {
       memccpy(dst_buff, colon + 1, found - colon - 1, found - colon - 1);
     }
     offset = found + 4 - in_buffer;
@@ -159,8 +187,9 @@ int filter_data(uint8_t *dst_buff, uint8_t *src_buff) {
   return 0;
 }
 
-int main(void) {
-  char out_buffer[UART_BUF_SIZE];
+int main(void)
+{
+  char out_buffer[UART_BUF_SIZE] = {0};
   int recv_count;
 
   USART_Init();
@@ -172,17 +201,16 @@ int main(void) {
   /* Wait until ESP boots. */
   _delay_ms(5000);
 
-  clear_buffer(in_buffer);
-  clear_buffer(out_buffer);
-
   /* Use the HW uart to send the config string */
   send_esp_config();
 
-  while (true) {
+  while (true)
+  {
     recv_count = filter_data(out_buffer, in_buffer);
-    if (recv_count > 0) {
+    if (recv_count > 0)
+    {
       send_string(out_buffer, softuart_send_byte);
-      clear_buffer(out_buffer);
+      memset(out_buffer, 0, UART_BUF_SIZE);
     }
     _delay_ms(100);
   }
